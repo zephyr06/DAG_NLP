206c503fb4255e82bcc1130f6f858bcee702f817
zephyr06 <sen.zephyr.wang@gmail.com>
2022-03-04 16:33:34 -0500

    modify MultiKeyFactor's dependency files for better portability

--- sources/MultiKeyFactor.h
+++ sources/MultiKeyFactor.h
@@ -1,6 +1,45 @@
 #pragma once
 
-#include "DeclareDAG.h"
+#pragma once
+
+#include <chrono>
+#include <unordered_map>
+#include <math.h>
+#include <algorithm>
+
+#include <Eigen/Dense>
+#include <Eigen/SparseCore>
+#include <dirent.h>
+#include <gtsam/base/numericalDerivative.h>
+#include <gtsam/geometry/Point3.h>
+#include <gtsam/inference/Key.h>
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/linear/VectorValues.h>
+#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/DoglegOptimizer.h>
+#include <gtsam/inference/FactorGraph.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/Values.h>
+
+#include <boost/function.hpp>
+
+#include "Parameters.h"
+#include "colormod.h"
+#include "testMy.h"
+#include "profilier.h"
+
+using namespace std;
+using namespace gtsam;
+
+// typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> MatrixDynamic;
+typedef Eigen::Matrix<double, Eigen::Dynamic, 1> VectorDynamic;
+// typedef Eigen::SparseMatrix<double, Eigen::ColMajor> SM_Dynamic;
+// typedef boost::function<VectorDynamic(const VectorDynamic &)> NormalErrorFunction1D;
+// typedef boost::function<VectorDynamic(const VectorDynamic &, const VectorDynamic &)> NormalErrorFunction2D;
+typedef boost::function<Vector(const Values &x)> LambdaMultiKey;
+
+typedef long long int LLint;
 
 using namespace DAG_SPACE;
 typedef std::vector<VectorDynamic> VVec;
