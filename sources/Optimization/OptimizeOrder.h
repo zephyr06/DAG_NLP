1781cf1cb6a342404417b702bbd564ffda1f3146
zephyr06 <sen.zephyr.wang@gmail.com>
2022-10-04 21:54:53 -0400

    add a local optimal case n5_v76

--- sources/Optimization/OptimizeOrder.h
+++ sources/Optimization/OptimizeOrder.h
@@ -102,6 +102,21 @@ namespace DAG_SPACE
         }
         // TODO: optimize the final schedule to reduce RTDA
 
+        JobOrder jobOrderCurr = statusPrev.jobOrder_;
+        jobOrderCurr.ChangeJobOrder(5, 0);
+        IterationStatus<SchedulingAlgorithm> statusCurr(dagTasks, tasksInfo, jobOrderCurr);
+        PrintSchedule(tasksInfo, statusCurr.startTimeVector_);
+        if (MakeProgress<SchedulingAlgorithm>(statusPrev, statusCurr))
+        {
+            findNewUpdate = true;
+            statusPrev = statusCurr;
+            if (debugMode == 1)
+            {
+                std::cout << "Make progress!" << std::endl;
+                PrintSchedule(tasksInfo, statusCurr.startTimeVector_);
+            }
+        }
+
         ScheduleResult scheduleRes{statusPrev.jobOrder_, statusPrev.startTimeVector_, statusPrev.schedulable_, statusPrev.maxRtda_};
         return scheduleRes;
     }
