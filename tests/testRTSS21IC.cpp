12aa1cf13edc35f025cb54f16aad9b960584c12d
zephyr06 <sen.zephyr.wang@gmail.com>
2022-10-14 16:45:39 -0400

    SF Factor in RTSSIC uses newer version now

--- tests/testRTSS21IC.cpp
+++ tests/testRTSS21IC.cpp
@@ -1,10 +1,11 @@
 #include <CppUnitLite/TestHarness.h>
 #include "sources/Tools/testMy.h"
-#include "sources/Baseline/RTSS21IC.h"
+#include "sources/Optimization/OptimizeWithElimination.h"
 TEST(DAG_Optimize_schedule, v1)
 {
     OrderOptDAG_SPACE::DAG_Model dagTasks = OrderOptDAG_SPACE::ReadDAG_Tasks(RTSS21IC_NLP::PROJECT_PATH + "TaskData/" + RTSS21IC_NLP::testDataSetName + ".csv", "orig");
-    OrderOptDAG_SPACE::ScheduleResult res = OrderOptDAG_SPACE::ScheduleRTSS21IC(dagTasks);
+    // OrderOptDAG_SPACE::ScheduleResult res = OrderOptDAG_SPACE::ScheduleRTSS21IC(dagTasks);
+    auto sth = RTSS21IC_NLP::DAG_SPACE::OptimizeScheduling(dagTasks);
 }
 int main()
 {
