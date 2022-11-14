99456b6a0e6521e3129195ec7e080c4ec3fcf98f
zephyr06 <sen.zephyr.wang@gmail.com>
2022-11-13 21:01:19 -0500

    remove several test files

--- tests/testSFOrder.cpp
+++ tests/testSFOrder.cpp
@@ -305,7 +305,35 @@ TEST(sched, v2)
     EXPECT_LONGS_EQUAL(1, processorJobVec_[3]);
     EXPECT(ExamBasic_Feasibility(dagTasks, tasksInfo, initialSTV, processorJobVec_, processorNum));
 }
+TEST(SFOrderSchedule, test1)
+{
+    using namespace OrderOptDAG_SPACE;
+    // DAG_Model dagTasks = ReadDAG_Tasks(PROJECT_PATH + "TaskData/" + testDataSetName + ".csv", "orig");
 
+    for (int taskSetId = 1; taskSetId <= 79; taskSetId++)
+    {
+        if (taskSetId == 41)
+        { // ignore task sets with double execution time
+            taskSetId = 45;
+        }
+        DAG_Model dagTasks = ReadDAG_Tasks(PROJECT_PATH + "TaskData/test_n5_v" + std::to_string(taskSetId) + ".csv", "orig");
+
+        TaskSet &tasks = dagTasks.tasks;
+        TaskSetInfoDerived tasksInfo(tasks);
+        VectorDynamic initialSTV = ListSchedulingLFTPA(dagTasks, tasksInfo, 2);
+        SFOrder sfOrder(tasksInfo, initialSTV);
+        auto startTimeVector = SFOrderScheduling(dagTasks, tasksInfo, 2, sfOrder);
+        if (debugMode)
+        {
+            std::cout << "taskSetId is : " << taskSetId << std::endl;
+            std::cout << "Initial schedule: " << std::endl;
+            PrintSchedule(tasksInfo, initialSTV);
+            std::cout << "SFOrder schedule: " << std::endl;
+            PrintSchedule(tasksInfo, startTimeVector);
+        }
+        AssertEigenEqualVector(initialSTV, startTimeVector);
+    }
+}
 TEST(SFOrder, insert_erase_v2)
 {
     OrderOptDAG_SPACE::DAG_Model dagTasks = ReadDAG_Tasks(PROJECT_PATH + "TaskData/test_n3_v9.csv", "orig");
