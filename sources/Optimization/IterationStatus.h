074ea07f9fdfa120360b666bfaba25b04bb449f6
zephyr06 <sen.zephyr.wang@gmail.com>
2022-11-15 20:29:03 -0500

    SFOrderOptSingle runs

--- sources/Optimization/IterationStatus.h
+++ sources/Optimization/IterationStatus.h
@@ -51,7 +51,7 @@ namespace OrderOptDAG_SPACE
                 weightPunish_ = weightInMpRTDAPunish;
             }
         };
-
+        static int infeasibleCount = 0;
         extern int infeasibleCount;
         template <typename OrderScheduler>
         struct IterationStatus
@@ -198,6 +198,23 @@ namespace OrderOptDAG_SPACE
         };
 
         template <typename OrderScheduler>
-        bool MakeProgress(IterationStatus<OrderScheduler> &statusPrev, IterationStatus<OrderScheduler> &statusCurr);
+        bool MakeProgress(IterationStatus<OrderScheduler> &statusPrev, IterationStatus<OrderScheduler> &statusCurr)
+        {
+            if (!statusCurr.schedulable_)
+            {
+                infeasibleCount++;
+                if (debugMode == 1)
+                {
+                    TaskSetInfoDerived tasksInfo(statusCurr.dagTasks_.tasks);
+                    std::cout << "Infeasible schedule #:" << infeasibleCount << std::endl;
+                    // PrintSchedule(tasksInfo, statusCurr.startTimeVector_);
+                    statusCurr.jobOrder_.print();
+                }
+                return false;
+            }
+            if (statusCurr.objWeighted_ < statusPrev.objWeighted_)
+                return true;
+            return false;
+        }
     }
 }
