1082bd090931790e6a1545e71415f516e1db9775
zephyr06 <sen.zephyr.wang@gmail.com>
2022-11-21 11:33:10 -0500

    update testOptSFOrderIntegration

--- tests/testOptSFOrderIntegration.cpp
+++ tests/testOptSFOrderIntegration.cpp
@@ -4,34 +4,39 @@
 using namespace OrderOptDAG_SPACE;
 using namespace OrderOptDAG_SPACE::OptimizeSF;
 
-class ObjExperimentObjTest1 : public ::testing::Test
+class DAGScheduleOptimizerTest1 : public ::testing::Test
 {
 protected:
     void SetUp() override
     {
-        dagTasks = ReadDAG_Tasks(PROJECT_PATH + "TaskData/test_n5_v1.csv", "orig"); // single-rate dag
+        dagTasks = ReadDAG_Tasks(PROJECT_PATH + "TaskData/test_n3_v18.csv", "orig"); // single-rate dag
         tasks = dagTasks.tasks;
         tasksInfo = TaskSetInfoDerived(tasks);
-        chain1 = {0, 1, 2};
+        chain1 = {0, 2};
         dagTasks.chains_[0] = chain1;
 
-        scheduleOptions.considerSensorFusion_ = 1;
+        double timeLimits = 1;
+        scheduleOptions.considerSensorFusion_ = 0;
         scheduleOptions.freshTol_ = 0;
         scheduleOptions.sensorFusionTolerance_ = 0;
         scheduleOptions.weightInMpRTDA_ = 0.5;
         scheduleOptions.weightInMpSf_ = 0.5;
         scheduleOptions.weightPunish_ = 10;
-    }
+
+        dagScheduleOptimizer = DAGScheduleOptimizer<SimpleOrderScheduler, RTDAExperimentObj>(dagTasks, scheduleOptions, timeLimits);
+    };
+
     DAG_Model dagTasks;
     TaskSet tasks;
     TaskSetInfoDerived tasksInfo;
     std::vector<int> chain1;
     ScheduleOptions scheduleOptions;
+    DAGScheduleOptimizer<SimpleOrderScheduler, RTDAExperimentObj> dagScheduleOptimizer;
 };
 
-TEST_F(ObjExperimentObjTest1, constructor)
+TEST_F(DAGScheduleOptimizerTest1, constructor)
 {
-    EXPECT_EQ(tasks.size(), 5);
+    EXPECT_EQ(tasks.size(), 3);
 }
 
 // TODO: write a failure test for OptimizeOrder
