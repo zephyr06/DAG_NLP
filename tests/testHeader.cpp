a68b732386d4422edb4e0676cf3ba1fd18160f88
zephyr06 <sen.zephyr.wang@gmail.com>
2022-11-24 10:55:42 -0500

    remove np-schedulability from CMakeLists, separate header and source files, pass all tests

--- tests/testHeader.cpp
+++ tests/testHeader.cpp
@@ -1,21 +1,7 @@
 #include <CppUnitLite/TestHarness.h>
 #include "sources/Utils/testMy.h"
-#include "sources/Utils/Parameters.h"
-#include "sources/Utils/MatirxConvenient.h"
-#include "sources/Utils/OptimizeOrderUtils.h"
-// #include "sources/Factors/RTDA_Factor.h"
-// #include "sources/Factors/Interval.h"
-// #include "sources/Optimization/TopologicalSort.h"
-// #include "sources/Baseline/VerucchiRTDABridge.h"
-// #include "sources/Utils/BatchUtils.h"
-// #include "sources/batchOptimizeSFOrder.h"
 
-#include <CppUnitLite/TestHarness.h>
-#include "sources/TaskModel/DAG_Model.h"
-#include "sources/Optimization/ScheduleSimulation.h"
-#include "sources/Baseline/RTSS21IC.h"
-#include "sources/Utils/profilier.h"
-#include "sources/Utils/testMy.h"
+#include "sources/batchOptimizeSFOrder.h"
 int main()
 {
     TestResult tr;
