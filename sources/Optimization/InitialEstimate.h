23254f7f6f22f75c4dcdc1394b9433e3cf133342
zephyr06 <sen.zephyr.wang@gmail.com>
2022-11-26 00:32:24 -0500

    add ExamBasic_Feasibility in SelectInitialFromPool, however, SelectInitialFromPool reduces overall performance

--- sources/Optimization/InitialEstimate.h
+++ sources/Optimization/InitialEstimate.h
@@ -9,28 +9,35 @@
 #include "sources/Optimization/ScheduleSimulation.h"
 #include "sources/Optimization/ObjectiveFunctions.h"
 #include "sources/Optimization/ScheduleOptions.h"
+#include "sources/Utils/OptimizeOrderUtils.h"
 
 using namespace RegularTaskSystem;
 namespace OrderOptDAG_SPACE
 {
     namespace OptimizeSF
     {
+        // processorIdVec is actually an argument that serves as output
         template <typename ObjectiveFunctionBase>
         VectorDynamic SelectInitialFromPool(const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo, const ScheduleOptions &scheduleOptions,
                                             boost::optional<std::vector<uint> &> processorIdVec = boost::none)
         {
-            VectorDynamic stvBest = ListSchedulingLFTPA(dagTasks, tasksInfo, scheduleOptions.processorNum_);
+            std::vector<uint> processorJobVec;
+            VectorDynamic stvBest = ListSchedulingLFTPA(dagTasks, tasksInfo, scheduleOptions.processorNum_, processorJobVec);
             double objMin = RTDAExperimentObj::TrueObj(dagTasks, tasksInfo, stvBest, scheduleOptions);
             for (double modifyCoeff = 0; modifyCoeff < 1000; modifyCoeff += 10)
             {
-                VectorDynamic stvCurr = ListSchedulingLFTPA(dagTasks, tasksInfo, scheduleOptions.processorNum_, processorIdVec, modifyCoeff);
+                std::vector<uint> processorJobVecCurr;
+                VectorDynamic stvCurr = ListSchedulingLFTPA(dagTasks, tasksInfo, scheduleOptions.processorNum_, processorJobVecCurr, modifyCoeff);
                 double objCurr = RTDAExperimentObj::TrueObj(dagTasks, tasksInfo, stvCurr, scheduleOptions);
-                if (objCurr < objMin && ExamBasic_Feasibility(dagTasks, tasksInfo, stvCurr, processorJobVec, scheduleOptions.processorNum_))
+                if (objCurr < objMin && ExamBasic_Feasibility(dagTasks, tasksInfo, stvCurr, processorJobVecCurr, scheduleOptions.processorNum_))
                 {
                     objMin = objCurr;
                     stvBest = stvCurr;
+                    processorJobVec = processorJobVecCurr;
                 }
             }
+            if (processorIdVec != boost::none)
+                *processorIdVec = processorJobVec;
             return stvBest;
         }
     }
