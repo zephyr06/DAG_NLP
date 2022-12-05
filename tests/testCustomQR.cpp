cff1e2cccb0dd2bdd01acab26a3513da80f4e6fe
zephyr06 <sen.zephyr.wang@gmail.com>
2022-12-04 22:16:42 -0500

    add ApplyQR

--- tests/testCustomQR.cpp
+++ tests/testCustomQR.cpp
@@ -39,6 +39,9 @@ protected:
         jobOrder = SFOrder(tasksInfo, initial);
         jobOrder.print();
         VectorDynamic initial2 = SFOrderScheduling(dagTasks.tasks, tasksInfo, scheduleOptions.processorNum_, jobOrder, processorJobVec);
+
+        m = GenerateMatrixDynamic(4, 3);
+        m << 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11;
     };
 
     double timeLimits = 1;
@@ -50,6 +53,7 @@ protected:
     std::vector<uint> processorJobVec;
     SFOrder jobOrder;
     VectorDynamic initial;
+    MatrixDynamic m;
 };
 
 TEST_F(DAGScheduleOptimizerTest1, QR_Eigen)
@@ -61,22 +65,51 @@ TEST_F(DAGScheduleOptimizerTest1, QR_Eigen)
     EXPECT_FLOAT_EQ(-5.39103, qrEigen.R.sum());
 }
 
-// Apply given rotations at row-basis; After applying it, the m(i, 0) will become 0
-inline Eigen::JacobiRotation<float> ApplyGivenRotations(MatrixDynamic &m, int rowIndex)
+struct Q_Operation
 {
     Eigen::JacobiRotation<float> G;
-    G.makeGivens(m(rowIndex - 1, 0), m(rowIndex, 0));
-    m.applyOnTheLeft(rowIndex - 1, rowIndex, G.adjoint());
-    return G;
+    int i;
+    int j;
+};
+// Apply given rotations at row-basis; After applying it, the m(i, j) will become 0
+inline Q_Operation ApplyGivenRotations(MatrixDynamic &m, int i, int j)
+{
+    Eigen::JacobiRotation<float> G;
+    G.makeGivens(m(i - 1, j), m(i, j));
+    m.applyOnTheLeft(i - 1, i, G.adjoint());
+    return Q_Operation{G, i, j};
 }
 TEST_F(DAGScheduleOptimizerTest1, makeGivens)
 {
-    MatrixDynamic m = GenerateMatrixDynamic(4, 3);
-    m << 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11;
-    auto g1 = ApplyGivenRotations(m, 3);
+    ApplyGivenRotations(m, 3, 0);
     EXPECT_NEAR(0, m(3, 0), 1e-5);
-    auto g2 = ApplyGivenRotations(m, 2);
+    ApplyGivenRotations(m, 2, 0);
     EXPECT_NEAR(0, m(2, 0), 1e-5);
+}
+
+std::vector<Q_Operation> ApplyQR(MatrixDynamic &mat)
+{
+    int m = mat.rows();
+    int n = mat.cols();
+    std::vector<Q_Operation> Qs;
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = m - 1; i > j; i--)
+        {
+            auto g = ApplyGivenRotations(mat, i, j);
+            Qs.push_back(g);
+        }
+    }
+    return Qs;
+}
+TEST_F(DAGScheduleOptimizerTest1, QR_Custom)
+{
+
+    QR qrEigen = GetEigenQR(m);
+    auto sth = ApplyQR(m);
+    std::cout << "R matrix after QR: " << std::endl
+              << m << std::endl;
+    EXPECT_NEAR(qrEigen.R.norm(), m.norm(), 1e-5);
 }
 
 int main(int argc, char **argv)
