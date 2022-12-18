d296d9187848f77faedd455541867c1a9a3a8802
zephyr06 <sen.zephyr.wang@gmail.com>
2022-12-18 12:33:02 -0500

    fix LPTest2 set-up, move to DirectLP

--- tests/testDirectLP.cpp
+++ tests/testDirectLP.cpp
@@ -12,6 +12,7 @@
 #include "sources/Optimization/JacobianAnalyze.h"
 #include "sources/Utils/IncrementQR.h"
 #include "sources/Optimization/LinearProgrammingSolver.h"
+#include "sources/Optimization/LPSolverCplex.h"
 
 using namespace OrderOptDAG_SPACE;
 using namespace OrderOptDAG_SPACE::OptimizeSF;
@@ -145,6 +146,90 @@ TEST_F(LPTest1, SolveLP)
     EXPECT_TRUE(gtsam::assert_equal(xExpect, xActual, 1e-4));
 }
 
+class LPTest2 : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        dagTasks = ReadDAG_Tasks(GlobalVariablesDAGOpt::PROJECT_PATH + "TaskData/test_n3_v11.csv", "orig", 1);
+        tasks = dagTasks.tasks;
+        tasksInfo = TaskSetInfoDerived(tasks);
+        // chain1 = {0, 2};
+        // dagTasks.chains_[0] = chain1;
+        timeLimits = 10;
+        scheduleOptions.processorNum_ = 2;
+        scheduleOptions.considerSensorFusion_ = 0;
+        scheduleOptions.freshTol_ = 0;
+        scheduleOptions.sensorFusionTolerance_ = 0;
+        scheduleOptions.weightInMpRTDA_ = 0.5;
+        scheduleOptions.weightInMpSf_ = 0.5;
+        scheduleOptions.weightPunish_ = 10;
+
+        initial = ListSchedulingLFTPA(dagTasks, tasksInfo, scheduleOptions.processorNum_, processorJobVec);
+        // std::cout << "Initial solutions: " << std::endl
+        //           << initial << std::endl
+        //           << std::endl;
+        jobOrder = SFOrder(tasksInfo, initial);
+        // jobOrder.print();
+
+        // std::vector<AugmentedJacobian> augJacobs = GetVariableBlocks(dagTasks, tasksInfo, jobOrder, processorJobVec, scheduleOptions.processorNum_);
+        // AugmentedJacobian jacobAll = MergeAugJacobian(augJacobs);
+        AugmentedJacobian jacobAll = GetDAGJacobianOrg(dagTasks, tasksInfo, jobOrder, processorJobVec, scheduleOptions.processorNum_);
+        A = jacobAll.jacobian;
+        As = A.sparseView();
+
+        // std::cout << "A" << std::endl
+        //           << A << std::endl
+        //           << std::endl;
+        b = jacobAll.rhs;
+        // std::cout << "rhs" << std::endl
+        //           << b << std::endl
+        //           << std::endl;
+        c = GenerateVectorDynamic(A.cols());
+        c(0) = 1;
+        c(1) = -1;
+        // std::cout << c << std::endl
+        //           << std::endl;
+    };
+
+    double timeLimits = 1;
+    DAG_Model dagTasks;
+    TaskSet tasks;
+    TaskSetInfoDerived tasksInfo;
+    std::vector<int> chain1;
+    ScheduleOptions scheduleOptions;
+    std::vector<uint> processorJobVec;
+    SFOrder jobOrder;
+    VectorDynamic initial;
+
+    MatrixDynamic A;
+    Eigen::SparseMatrix<double> As;
+    VectorDynamic b;
+    VectorDynamic c;
+};
+
+TEST_F(LPTest2, formulation_feasible)
+{
+    ASSERT_THAT((b - A * initial).minCoeff(), testing::Ge(0));
+}
+
+TEST_F(LPTest2, SolveLP_Cplex)
+{
+    TimerFunc _;
+    VectorDynamic xExpect(2);
+    xExpect << 0, 19;
+    VectorDynamic xActual = SolveLP_Cplex(As, b, c);
+    EXPECT_TRUE(gtsam::assert_equal(xExpect, xActual.block(0, 0, 2, 1), 1e-4));
+}
+
+TEST_F(LPTest2, SolveLP)
+{
+    TimerFunc _;
+    VectorDynamic xExpect(2);
+    xExpect << 0, 19;
+    VectorDynamic xActual = SolveLP(As, b, c);
+    EXPECT_TRUE(gtsam::assert_equal(xExpect, xActual.block(0, 0, 2, 1), 1e-4));
+}
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
