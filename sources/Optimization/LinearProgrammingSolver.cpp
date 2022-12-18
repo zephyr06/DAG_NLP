0e1de2d4bde1e3a5b92d5fce6b985b45bed760b4
zephyr06 <sen.zephyr.wang@gmail.com>
2022-12-18 13:38:49 -0500

    add testLPForDAG, not papss

--- sources/Optimization/LinearProgrammingSolver.cpp
+++ sources/Optimization/LinearProgrammingSolver.cpp
@@ -1,178 +1,170 @@
 #include "sources/Optimization/LinearProgrammingSolver.h"
 // TODO: if there is time, consider using band matrix from eigen
-namespace LPOptimizer
-{
-    LPData::LPData(const Eigen::SparseMatrix<double> &A, const VectorDynamic &b, const VectorDynamic &c) : b_(b), m_(A.rows()), n_(A.cols())
-    {
-        if (m_ > n_)
-        {
-            // A_.resize(m_, n_ + m_);
-            A_ = Eigen::SparseMatrix<double>(m_, m_ + n_);
-            A_.reserve(A.nonZeros() + m_);
+namespace LPOptimizer {
+LPData::LPData(const Eigen::SparseMatrix<double> &A, const VectorDynamic &b, const VectorDynamic &c)
+    : b_(b), m_(A.rows()), n_(A.cols()) {
+  if (m_ > n_) {
+    // A_.resize(m_, n_ + m_);
+    A_ = Eigen::SparseMatrix<double>(m_, m_ + n_);
+    A_.reserve(A.nonZeros() + m_);
 
-            for (uint c = 0; c < A.cols(); ++c)
-            {
-                A_.startVec(c); // Important: Must be called once for each column before inserting!
-                for (Eigen::SparseMatrix<double>::InnerIterator itL(A, c); itL; ++itL)
-                    A_.insertBack(itL.row(), c) = itL.value();
-            }
-            A_.finalize();
-            for (uint i = 0; i < m_; i++)
-                A_.insert(i, i + n_) = 1;
+    for (uint c = 0; c < A.cols(); ++c) {
+      A_.startVec(c); // Important: Must be called once for each column before inserting!
+      for (Eigen::SparseMatrix<double>::InnerIterator itL(A, c); itL; ++itL)
+        A_.insertBack(itL.row(), c) = itL.value();
+    }
+    A_.finalize();
+    for (uint i = 0; i < m_; i++)
+      A_.insert(i, i + n_) = 1;
 
-            c_ = Eigen::MatrixXd(m_ + n_, 1);
-            VectorDynamic zeros = GenerateVectorDynamic(m_);
-            c_ << c,
-                zeros;
-        }
-        else
-        {
-            CoutError("A's dimension is wrong in LPData!");
-        }
-        centralVarCurr_ = GenerateInitialLP();
+    c_ = Eigen::MatrixXd(m_ + n_, 1);
+    VectorDynamic zeros = GenerateVectorDynamic(m_);
+    c_ << c, zeros;
+  } else {
+    CoutError("A's dimension is wrong in LPData!");
+  }
+  centralVarCurr_ = GenerateInitialLP();
+}
+
+CentralVariable LPData::GenerateInitialLP() {
+  Eigen::SparseMatrix<double> AA = A_ * A_.transpose();
+  Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> AAFact(AA);
+  // auto AAFact = solver.compute(AA);
+  VectorDynamic xCurr_ = A_.transpose() * (AAFact.solve(b_));
+  VectorAdd(xCurr_, std::max(-1.5 * xCurr_.minCoeff(), 0.0));
+
+  VectorDynamic lambdaCurr_ = AAFact.solve(A_ * c_);
+  VectorDynamic sCurr_ = c_ - A_.transpose() * lambdaCurr_;
+  VectorAdd(sCurr_, std::max(-1.5 * sCurr_.minCoeff(), 0.0));
+  double deltax = 0.5 * (xCurr_.transpose() * sCurr_)(0, 0) / sCurr_.sum() / 2.0;
+  double deltas = 0.5 * (xCurr_.transpose() * sCurr_)(0, 0) / xCurr_.sum() / 2.0;
+  VectorAdd(xCurr_, deltax);
+  VectorAdd(sCurr_, deltas);
+
+  return CentralVariable{xCurr_, sCurr_, lambdaCurr_};
+}
+
+double GetAlphaAff(const VectorDynamic &xCurr, const VectorDynamic &xDelta) {
+  size_t m = xDelta.rows();
+  double minDelta = 1e9;
+  // TODO: use an iterator instead
+  for (uint i = 0; i < m; i++) {
+    if (xDelta(i) < 0) {
+      minDelta = std::min(minDelta, -1 * xCurr(i) / xDelta(i));
+    }
+  }
+  return std::min(1.0, minDelta);
+}
+
+CentralVariable LPData::SolveLinearSystem() {
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> S = centralVarCurr_.s.asDiagonal();
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> X = centralVarCurr_.x.asDiagonal();
+  VectorDynamic rb = A_ * centralVarCurr_.x - b_;
+  VectorDynamic rc = A_.transpose() * centralVarCurr_.lambda + centralVarCurr_.s - c_;
+  VectorDynamic rxs1 = X * centralVarCurr_.s;
+  VectorDynamic sInv = centralVarCurr_.s;
+  for (uint i = 0; i < sInv.rows(); i++)
+    sInv(i) = 1 / sInv(i);
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> S_inv = sInv.asDiagonal();
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> D2 = (S_inv * centralVarCurr_.x).asDiagonal();
+  Eigen::SparseMatrix<double> AA = A_ * D2 * A_.transpose();
+  Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> AAFact(AA);
+
+  // predictor
+  CentralVariable centralDelta;
+  centralDelta.lambda = AAFact.solve(-1 * rb - A_ * X * S_inv * rc + A_ * (S_inv * rxs1));
+  centralDelta.s = -1 * rc - A_.transpose() * centralDelta.lambda;
+  centralDelta.x = -1 * S_inv * rxs1 - X * (S_inv * centralDelta.s);
+
+  // corrector
+  double alphaAffPrim = GetAlphaAff(centralVarCurr_.x, centralDelta.x);
+  double alphaAffDual = GetAlphaAff(centralVarCurr_.s, centralDelta.s);
+  double muAff = ((centralVarCurr_.x + centralDelta.x * alphaAffPrim).transpose() *
+                  (centralVarCurr_.s + centralDelta.s * alphaAffDual))(0, 0) /
+                 centralVarCurr_.x.rows();
+  double sigma = std::pow(muAff / Duality(), 2);
+
+  VectorDynamic rxs2 = -1 * (-1 * X * centralVarCurr_.s - centralDelta.x.asDiagonal() * centralDelta.s);
+  VectorAdd(rxs2, Duality() * sigma * -1);
+
+  centralDelta.lambda = AAFact.solve(-1 * rb - A_ * X * S_inv * rc + A_ * (S_inv * rxs2));
+  centralDelta.s = -1 * rc - A_.transpose() * centralDelta.lambda;
+  centralDelta.x = -1 * S_inv * rxs2 - X * (S_inv * centralDelta.s);
+
+  return centralDelta;
+}
+
+void LPData::ApplyCentralDelta(const CentralVariable &centralDelta, double eta) {
+  double alphaAffPrim = GetAlphaAff(centralVarCurr_.x, centralDelta.x);
+  double alphaAffDual = GetAlphaAff(centralVarCurr_.s, centralDelta.s);
+  centralVarCurr_.x = centralVarCurr_.x + centralDelta.x * std::min(1.0, alphaAffPrim * eta);
+  centralVarCurr_.lambda = centralVarCurr_.lambda + centralDelta.lambda * std::min(1.0, alphaAffDual * eta);
+  centralVarCurr_.s = centralVarCurr_.s + centralDelta.s * std::min(1.0, alphaAffDual * eta);
+}
+
+VectorDynamic SolveLP(const Eigen::SparseMatrix<double> &A, const VectorDynamic &b, const VectorDynamic &c,
+                      double precision) {
+  LPData lpData(A, b, c);
+  return SolveLP(lpData, precision);
+}
+
+VectorDynamic SolveLP(LPData &lpData, double precision) {
+  if (lpData.centralVarCurr_.x.rows() == 0)
+    lpData.centralVarCurr_ = lpData.GenerateInitialLP();
+  int iterationCount = 0;
+  while (lpData.Duality() > precision && iterationCount < 1000) {
+    if (GlobalVariablesDAGOpt::debugMode == 1) {
+      std::cout << "Current duality measure: " << lpData.Duality() << std::endl;
     }
 
-    CentralVariable LPData::GenerateInitialLP()
-    {
-        Eigen::SparseMatrix<double> AA = A_ * A_.transpose();
-        Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> AAFact(AA);
-        // auto AAFact = solver.compute(AA);
-        VectorDynamic xCurr_ = A_.transpose() * (AAFact.solve(b_));
-        VectorAdd(xCurr_, std::max(-1.5 * xCurr_.minCoeff(), 0.0));
-
-        VectorDynamic lambdaCurr_ = AAFact.solve(A_ * c_);
-        VectorDynamic sCurr_ = c_ - A_.transpose() * lambdaCurr_;
-        VectorAdd(sCurr_, std::max(-1.5 * sCurr_.minCoeff(), 0.0));
-        double deltax = 0.5 * (xCurr_.transpose() * sCurr_)(0, 0) / sCurr_.sum() / 2.0;
-        double deltas = 0.5 * (xCurr_.transpose() * sCurr_)(0, 0) / xCurr_.sum() / 2.0;
-        VectorAdd(xCurr_, deltax);
-        VectorAdd(sCurr_, deltas);
-
-        return CentralVariable{xCurr_, sCurr_, lambdaCurr_};
-    }
-
-    double GetAlphaAff(const VectorDynamic &xCurr, const VectorDynamic &xDelta)
-    {
-        size_t m = xDelta.rows();
-        double minDelta = 1e9;
-        // TODO: use an iterator instead
-        for (uint i = 0; i < m; i++)
-        {
-            if (xDelta(i) < 0)
-            {
-                minDelta = std::min(minDelta, -1 * xCurr(i) / xDelta(i));
-            }
-        }
-        return std::min(1.0, minDelta);
-    }
-
-    CentralVariable LPData::SolveLinearSystem()
-    {
-        Eigen::DiagonalMatrix<double, Eigen::Dynamic> S = centralVarCurr_.s.asDiagonal();
-        Eigen::DiagonalMatrix<double, Eigen::Dynamic> X = centralVarCurr_.x.asDiagonal();
-        VectorDynamic rb = A_ * centralVarCurr_.x - b_;
-        VectorDynamic rc = A_.transpose() * centralVarCurr_.lambda + centralVarCurr_.s - c_;
-        VectorDynamic rxs1 = X * centralVarCurr_.s;
-        VectorDynamic sInv = centralVarCurr_.s;
-        for (uint i = 0; i < sInv.rows(); i++)
-            sInv(i) = 1 / sInv(i);
-        Eigen::DiagonalMatrix<double, Eigen::Dynamic> S_inv = sInv.asDiagonal();
-        Eigen::DiagonalMatrix<double, Eigen::Dynamic> D2 = (S_inv * centralVarCurr_.x).asDiagonal();
-        Eigen::SparseMatrix<double> AA = A_ * D2 * A_.transpose();
-        Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> AAFact(AA);
-
-        // predictor
-        CentralVariable centralDelta;
-        centralDelta.lambda = AAFact.solve(-1 * rb - A_ * X * S_inv * rc + A_ * (S_inv * rxs1));
-        centralDelta.s = -1 * rc - A_.transpose() * centralDelta.lambda;
-        centralDelta.x = -1 * S_inv * rxs1 - X * (S_inv * centralDelta.s);
-
-        // corrector
-        double alphaAffPrim = GetAlphaAff(centralVarCurr_.x, centralDelta.x);
-        double alphaAffDual = GetAlphaAff(centralVarCurr_.s, centralDelta.s);
-        double muAff = ((centralVarCurr_.x + centralDelta.x * alphaAffPrim).transpose() * (centralVarCurr_.s + centralDelta.s * alphaAffDual))(0, 0) / centralVarCurr_.x.rows();
-        double sigma = std::pow(muAff / Duality(), 2);
-
-        VectorDynamic rxs2 = -1 * (-1 * X * centralVarCurr_.s - centralDelta.x.asDiagonal() * centralDelta.s);
-        VectorAdd(rxs2, Duality() * sigma * -1);
-
-        centralDelta.lambda = AAFact.solve(-1 * rb - A_ * X * S_inv * rc + A_ * (S_inv * rxs2));
-        centralDelta.s = -1 * rc - A_.transpose() * centralDelta.lambda;
-        centralDelta.x = -1 * S_inv * rxs2 - X * (S_inv * centralDelta.s);
-
-        return centralDelta;
-    }
-
-    void LPData::ApplyCentralDelta(const CentralVariable &centralDelta, double eta)
-    {
-        double alphaAffPrim = GetAlphaAff(centralVarCurr_.x, centralDelta.x);
-        double alphaAffDual = GetAlphaAff(centralVarCurr_.s, centralDelta.s);
-        centralVarCurr_.x = centralVarCurr_.x + centralDelta.x * std::min(1.0, alphaAffPrim * eta);
-        centralVarCurr_.lambda = centralVarCurr_.lambda + centralDelta.lambda * std::min(1.0, alphaAffDual * eta);
-        centralVarCurr_.s = centralVarCurr_.s + centralDelta.s * std::min(1.0, alphaAffDual * eta);
-    }
-
-    VectorDynamic SolveLP(const Eigen::SparseMatrix<double> &A, const VectorDynamic &b, const VectorDynamic &c, double precision)
-    {
-        LPData lpData(A, b, c);
-        return SolveLP(lpData, precision);
-    }
-
-    VectorDynamic SolveLP(LPData &lpData, double precision)
-    {
-        int iterationCount = 0;
-        while (lpData.Duality() > precision && iterationCount < 1000)
-        {
-            if (GlobalVariablesDAGOpt::debugMode == 1)
-            {
-                std::cout << "Current duality measure: " << lpData.Duality() << std::endl;
-            }
-
-            CentralVariable centralDelta = lpData.SolveLinearSystem();
-            lpData.ApplyCentralDelta(centralDelta, 0.9);
-            iterationCount++;
-        }
-        return lpData.centralVarCurr_.x.block(0, 0, lpData.n_, 1);
-    }
+    CentralVariable centralDelta = lpData.SolveLinearSystem();
+    lpData.ApplyCentralDelta(centralDelta, 0.9);
+    iterationCount++;
+  }
+  return lpData.centralVarCurr_.x.block(0, 0, lpData.n_, 1);
+}
 
 } // namespace LPOptimizer
 
-namespace OrderOptDAG_SPACE
-{
-    using namespace LPOptimizer;
+namespace OrderOptDAG_SPACE {
+using namespace LPOptimizer;
 
-    LPData GenerateRTDALPOrg(const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo, SFOrder &jobOrder, const std::vector<uint> processorJobVec, int processorNum)
-    {
-        AugmentedJacobian augJacobConstraints = GetDAGJacobianOrg(dagTasks, tasksInfo, jobOrder, processorJobVec, processorNum);
+LPData GenerateRTDALPOrg(const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo, SFOrder &jobOrder,
+                         const std::vector<uint> processorJobVec, int processorNum) {
+  AugmentedJacobian augJacobConstraints =
+      GetDAGJacobianOrg(dagTasks, tasksInfo, jobOrder, processorJobVec, processorNum);
 
-        AugmentedJacobian jacobAll;
-        int mAllMax = augJacobConstraints.jacobian.rows(), nAll = augJacobConstraints.jacobian.cols() + 2 * dagTasks.chains_.size();
-        for (uint i = 0; i < dagTasks.chains_.size(); i++)
-        {
-            mAllMax += 2 * (tasksInfo.hyperPeriod / dagTasks.tasks[dagTasks.chains_[i][0]].period + 1 + 1); // +1 just to be safe
-        }
-        jacobAll.jacobian.conservativeResize(mAllMax, nAll);
-        jacobAll.rhs.conservativeResize(mAllMax, 1);
-        jacobAll.jacobian.setZero();
+  AugmentedJacobian jacobAll;
+  int mAllMax = augJacobConstraints.jacobian.rows(),
+      nAll = augJacobConstraints.jacobian.cols() + 2 * dagTasks.chains_.size();
+  for (uint i = 0; i < dagTasks.chains_.size(); i++) {
+    mAllMax += 2 * (tasksInfo.hyperPeriod / dagTasks.tasks[dagTasks.chains_[i][0]].period + 1 +
+                    1); // +1 just to be safe
+  }
+  jacobAll.jacobian.conservativeResize(mAllMax, nAll);
+  jacobAll.rhs.conservativeResize(mAllMax, 1);
+  jacobAll.jacobian.setZero();
 
-        AugmentedJacobian jacobRTDAS = MergeJacobianOfRTDAChains(dagTasks, tasksInfo, jobOrder, processorJobVec, processorNum);
+  AugmentedJacobian jacobRTDAS =
+      MergeJacobianOfRTDAChains(dagTasks, tasksInfo, jobOrder, processorJobVec, processorNum);
 
-        jacobAll.jacobian.block(0, 0, augJacobConstraints.jacobian.rows(), augJacobConstraints.jacobian.cols()) = augJacobConstraints.jacobian;
-        jacobAll.jacobian.block(augJacobConstraints.jacobian.rows(), 0, jacobRTDAS.jacobian.rows(), jacobRTDAS.jacobian.cols()) = jacobRTDAS.jacobian;
+  jacobAll.jacobian.block(0, 0, augJacobConstraints.jacobian.rows(), augJacobConstraints.jacobian.cols()) =
+      augJacobConstraints.jacobian;
+  jacobAll.jacobian.block(augJacobConstraints.jacobian.rows(), 0, jacobRTDAS.jacobian.rows(),
+                          jacobRTDAS.jacobian.cols()) = jacobRTDAS.jacobian;
 
-        jacobAll.rhs.block(0, 0, augJacobConstraints.jacobian.rows(), 1) = augJacobConstraints.rhs;
-        jacobAll.rhs.block(augJacobConstraints.jacobian.rows(), 0, jacobRTDAS.jacobian.rows(), 1) = jacobRTDAS.rhs;
+  jacobAll.rhs.block(0, 0, augJacobConstraints.jacobian.rows(), 1) = augJacobConstraints.rhs;
+  jacobAll.rhs.block(augJacobConstraints.jacobian.rows(), 0, jacobRTDAS.jacobian.rows(), 1) = jacobRTDAS.rhs;
 
-        jacobAll.jacobian.conservativeResize(augJacobConstraints.jacobian.rows() + jacobRTDAS.jacobian.rows(), jacobAll.jacobian.cols());
-        jacobAll.rhs.conservativeResize(jacobAll.jacobian.rows(), 1);
+  jacobAll.jacobian.conservativeResize(augJacobConstraints.jacobian.rows() + jacobRTDAS.jacobian.rows(),
+                                       jacobAll.jacobian.cols());
+  jacobAll.rhs.conservativeResize(jacobAll.jacobian.rows(), 1);
 
-        LPData lpData;
-        lpData.A_ = jacobAll.jacobian.sparseView();
-        lpData.b_ = jacobAll.rhs;
-        lpData.c_ = GenerateVectorDynamic(nAll);
-        for (int i = augJacobConstraints.jacobian.cols(); i < nAll; i++)
-            lpData.c_(i) = 1;
-        return lpData;
-    }
+  VectorDynamic c = GenerateVectorDynamic(nAll);
+  for (int i = augJacobConstraints.jacobian.cols(); i < nAll; i++)
+    c(i) = 1;
+  LPData lpData(jacobAll.jacobian.sparseView(), jacobAll.rhs, c);
+  return lpData;
+}
 
 } // namespace OrderOptDAG_SPACE
---
+++ tests/testLPForDAG.cpp
@@ -0,0 +1,80 @@
+#include "gtsam/base/Testable.h" // assert_equal
+#include "ilcplex/cplex.h"
+#include "ilcplex/ilocplex.h"
+#include "gmock/gmock.h" // Brings in gMock.
+#include <Eigen/Core>
+#include <Eigen/Jacobi>
+#include <Eigen/SparseCore>
+#include <gtest/gtest.h>
+
+#include "sources/Optimization/JacobianAnalyze.h"
+#include "sources/Optimization/LPSolverCplex.h"
+#include "sources/Optimization/LinearProgrammingSolver.h"
+#include "sources/Optimization/OrderScheduler.h"
+#include "sources/TaskModel/DAG_Model.h"
+#include "sources/Utils/IncrementQR.h"
+
+using namespace OrderOptDAG_SPACE;
+using namespace OrderOptDAG_SPACE::OptimizeSF;
+using namespace GlobalVariablesDAGOpt;
+using namespace LPOptimizer;
+
+class DAGScheduleOptimizerTest1 : public ::testing::Test {
+protected:
+  void SetUp() override {
+    dagTasks = ReadDAG_Tasks(GlobalVariablesDAGOpt::PROJECT_PATH + "TaskData/"
+                                                                   "test_n3_v18.csv",
+                             "orig", 1);
+    tasks = dagTasks.tasks;
+    tasksInfo = TaskSetInfoDerived(tasks);
+    chain1 = {0, 2};
+    dagTasks.chains_[0] = chain1;
+    timeLimits = 1;
+    scheduleOptions.processorNum_ = 2;
+    scheduleOptions.considerSensorFusion_ = 0;
+    scheduleOptions.freshTol_ = 0;
+    scheduleOptions.sensorFusionTolerance_ = 0;
+    scheduleOptions.weightInMpRTDA_ = 0.5;
+    scheduleOptions.weightInMpSf_ = 0.5;
+    scheduleOptions.weightPunish_ = 10;
+
+    initial = GenerateVectorDynamic(4);
+    initial << 0, 10, 15, 12;
+    jobOrder = SFOrder(tasksInfo, initial);
+    jobOrder.print();
+    xOpt = GenerateVectorDynamic(4);
+    xOpt << 9, 10, 18, 11;
+  };
+
+  double timeLimits = 1;
+  DAG_Model dagTasks;
+  TaskSet tasks;
+  TaskSetInfoDerived tasksInfo;
+  std::vector<int> chain1;
+  ScheduleOptions scheduleOptions;
+  std::vector<uint> processorJobVec;
+  SFOrder jobOrder;
+  VectorDynamic initial;
+  VectorDynamic xOpt;
+};
+
+VectorDynamic OptRTDA_IPM(const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo, SFOrder &jobOrder,
+                          const std::vector<uint> processorJobVec, int processorNum) {
+
+  LPData lpData = GenerateRTDALPOrg(dagTasks, tasksInfo, jobOrder, processorJobVec, processorNum);
+  return SolveLP(lpData);
+}
+
+TEST_F(DAGScheduleOptimizerTest1, OptRTDA_IPM) {
+  VectorDynamic startOpt =
+      LPOrderScheduler::schedule(dagTasks, tasksInfo, scheduleOptions, jobOrder, processorJobVec);
+  std::cout << startOpt << std::endl;
+  EXPECT_TRUE(gtsam::assert_equal(xOpt, startOpt));
+  VectorDynamic startFromIPM =
+      OptRTDA_IPM(dagTasks, tasksInfo, jobOrder, processorJobVec, scheduleOptions.processorNum_);
+  EXPECT_TRUE(gtsam::assert_equal(xOpt, startFromIPM));
+}
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
