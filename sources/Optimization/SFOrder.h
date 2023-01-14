c3f27622e2669420baac44ac115afd6ba7de7427
zephyr06 <sen.zephyr.wang@gmail.com>
2023-01-13 19:38:53 -0500

    add testFindGlobal.cpp

--- sources/Optimization/SFOrder.h
+++ sources/Optimization/SFOrder.h
@@ -1,141 +1,126 @@
 #pragma once
 
-#include "unordered_set"
 #include "unordered_map"
+#include "unordered_set"
 
-#include "sources/Utils/Parameters.h"
-#include "sources/Utils/MatirxConvenient.h"
 #include "sources/TaskModel/DAG_Model.h"
 #include "sources/Utils/JobCEC.h"
+#include "sources/Utils/MatirxConvenient.h"
+#include "sources/Utils/Parameters.h"
 
-namespace OrderOptDAG_SPACE
+namespace OrderOptDAG_SPACE {
+
+class TimeInstance {
+private:
+  double time;
+
+public:
+  char type; // 's' or 'f'
+  JobCEC job;
+  TimeInstance(char type, JobCEC j, double t) : time(t), type(type), job(j) {}
+  TimeInstance(char type, JobCEC j) : time(-1), type(type), job(j) {}
+  double getTime() const { return time; }
+  char getType() const { return type; }
+
+  inline bool operator==(const TimeInstance &b) const { return type == b.type && job == b.job; }
+  inline bool operator!=(const TimeInstance &b) const { return !(*this == b); }
+};
+
+bool compareTimeInstance(const TimeInstance i1, const TimeInstance i2);
+
+class SFOrder {
+private:
+  TaskSetInfoDerived tasksInfo_;
+  struct SFPair {
+    LLint startInstanceIndex;
+    LLint finishInstanceIndex;
+    SFPair() : startInstanceIndex(-1), finishInstanceIndex(-1) {}
+  };
+  std::unordered_map<JobCEC, SFPair> jobSFMap_;
+
+public:
+  std::vector<TimeInstance> instanceOrder_;
+  bool whetherSFMapNeedUpdate;
+  SFOrder() {}
+
+  // O(n log(n))
+  SFOrder(const TaskSetInfoDerived &tasksInfo, VectorDynamic &startTimeVector) : tasksInfo_(tasksInfo) {
+
+    BeginTimer(__FUNCTION__);
+    instanceOrder_.reserve(tasksInfo.length * 2);
+    for (int i = 0; i < tasksInfo.N; i++) {
+      for (uint j = 0; j < tasksInfo.sizeOfVariables[i]; j++) {
+        JobCEC job(i, j);
+        TimeInstance instS('s', job, GetStartTime(job, startTimeVector, tasksInfo));
+        instanceOrder_.push_back(instS);
+        TimeInstance instF('f', job, GetFinishTime(job, startTimeVector, tasksInfo));
+        instanceOrder_.push_back(instF);
+      }
+    }
+
+    std::sort(instanceOrder_.begin(), instanceOrder_.end(), compareTimeInstance);
+
+    whetherSFMapNeedUpdate = true;
+    EstablishJobSFMap();
+    EndTimer(__FUNCTION__);
+  }
+
+  SFOrder(const TaskSetInfoDerived &tasksInfo, std::vector<TimeInstance> &instanceOrder)
+      : tasksInfo_(tasksInfo) {
+    instanceOrder_ = instanceOrder;
+    whetherSFMapNeedUpdate = true;
+  }
+
+  // O(n)
+  void EstablishJobSFMap();
+
+  LLint size() const { return instanceOrder_.size(); }
+  TimeInstance operator[](LLint index) {
+    RangeCheck(index);
+    return instanceOrder_[index];
+  }
+  TimeInstance at(LLint index) const {
+    RangeCheck(index);
+    TimeInstance inst = instanceOrder_[index];
+    return inst;
+  }
+  LLint GetJobStartInstancePosition(const JobCEC &job) {
+    EstablishJobSFMap();
+    if (job.jobId >= tasksInfo_.sizeOfVariables[job.taskId]) {
+      JobCEC jobWithinSingleHP{job.taskId, job.jobId % tasksInfo_.sizeOfVariables[job.taskId]};
+      return jobSFMap_.at(jobWithinSingleHP).startInstanceIndex +
+             job.jobId / tasksInfo_.sizeOfVariables[job.taskId] * tasksInfo_.length * 2;
+    } else
+      return jobSFMap_.at(job).startInstanceIndex;
+  }
+  LLint GetJobFinishInstancePosition(const JobCEC &job) {
+    EstablishJobSFMap();
+    if (job.jobId >= tasksInfo_.sizeOfVariables[job.taskId]) {
+      JobCEC jobWithinSingleHP{job.taskId, job.jobId % tasksInfo_.sizeOfVariables[job.taskId]};
+      return jobSFMap_.at(jobWithinSingleHP).finishInstanceIndex +
+             job.jobId / tasksInfo_.sizeOfVariables[job.taskId] * tasksInfo_.length * 2;
+    } else
+      return jobSFMap_.at(job).finishInstanceIndex;
+  }
+
+  void RangeCheck(LLint index, bool allowEnd = false) const;
+
+  void RemoveJob(JobCEC job);
+
+  void InsertStart(JobCEC job, LLint position);
+
+  void InsertFinish(JobCEC job, LLint position);
+
+  void RemoveFinish(JobCEC job, LLint position);
+
+  void print();
+};
+
+struct JobGroupRange // include the minIndex, but not the maxIndex
 {
-
-    class TimeInstance
-    {
-    private:
-        double time;
-
-    public:
-        char type; // 's' or 'f'
-        JobCEC job;
-        TimeInstance(char type, JobCEC j, double t) : time(t), type(type), job(j) {}
-        TimeInstance(char type, JobCEC j) : time(-1), type(type), job(j) {}
-        double getTime() const
-        {
-            return time;
-        }
-        char getType() const
-        {
-            return type;
-        }
-    };
-    bool compareTimeInstance(const TimeInstance i1, const TimeInstance i2);
-
-    class SFOrder
-    {
-    private:
-        TaskSetInfoDerived tasksInfo_;
-        struct SFPair
-        {
-            LLint startInstanceIndex;
-            LLint finishInstanceIndex;
-            SFPair() : startInstanceIndex(-1), finishInstanceIndex(-1) {}
-        };
-        std::unordered_map<JobCEC, SFPair> jobSFMap_;
-
-    public:
-        std::vector<TimeInstance> instanceOrder_;
-        bool whetherSFMapNeedUpdate;
-        SFOrder() {}
-
-        // O(n log(n))
-        SFOrder(const TaskSetInfoDerived &tasksInfo, VectorDynamic &startTimeVector) : tasksInfo_(tasksInfo)
-        {
-
-            BeginTimer(__FUNCTION__);
-            instanceOrder_.reserve(tasksInfo.length * 2);
-            for (int i = 0; i < tasksInfo.N; i++)
-            {
-                for (uint j = 0; j < tasksInfo.sizeOfVariables[i]; j++)
-                {
-                    JobCEC job(i, j);
-                    TimeInstance instS('s', job, GetStartTime(job, startTimeVector, tasksInfo));
-                    instanceOrder_.push_back(instS);
-                    TimeInstance instF('f', job, GetFinishTime(job, startTimeVector, tasksInfo));
-                    instanceOrder_.push_back(instF);
-                }
-            }
-
-            std::sort(instanceOrder_.begin(), instanceOrder_.end(), compareTimeInstance);
-
-            whetherSFMapNeedUpdate = true;
-            EstablishJobSFMap();
-            EndTimer(__FUNCTION__);
-        }
-
-        SFOrder(const TaskSetInfoDerived &tasksInfo, std::vector<TimeInstance> &instanceOrder) : tasksInfo_(tasksInfo)
-        {
-            instanceOrder_ = instanceOrder;
-            whetherSFMapNeedUpdate = true;
-        }
-
-        // O(n)
-        void EstablishJobSFMap();
-
-        LLint size() const { return instanceOrder_.size(); }
-        TimeInstance operator[](LLint index)
-        {
-            RangeCheck(index);
-            return instanceOrder_[index];
-        }
-        TimeInstance at(LLint index) const
-        {
-            RangeCheck(index);
-            TimeInstance inst = instanceOrder_[index];
-            return inst;
-        }
-        LLint GetJobStartInstancePosition(const JobCEC &job)
-        {
-            EstablishJobSFMap();
-            if (job.jobId >= tasksInfo_.sizeOfVariables[job.taskId])
-            {
-                JobCEC jobWithinSingleHP{job.taskId, job.jobId % tasksInfo_.sizeOfVariables[job.taskId]};
-                return jobSFMap_.at(jobWithinSingleHP).startInstanceIndex + job.jobId / tasksInfo_.sizeOfVariables[job.taskId] * tasksInfo_.length * 2;
-            }
-            else
-                return jobSFMap_.at(job).startInstanceIndex;
-        }
-        LLint GetJobFinishInstancePosition(const JobCEC &job)
-        {
-            EstablishJobSFMap();
-            if (job.jobId >= tasksInfo_.sizeOfVariables[job.taskId])
-            {
-                JobCEC jobWithinSingleHP{job.taskId, job.jobId % tasksInfo_.sizeOfVariables[job.taskId]};
-                return jobSFMap_.at(jobWithinSingleHP).finishInstanceIndex + job.jobId / tasksInfo_.sizeOfVariables[job.taskId] * tasksInfo_.length * 2;
-            }
-            else
-                return jobSFMap_.at(job).finishInstanceIndex;
-        }
-
-        void RangeCheck(LLint index, bool allowEnd = false) const;
-
-        void RemoveJob(JobCEC job);
-
-        void InsertStart(JobCEC job, LLint position);
-
-        void InsertFinish(JobCEC job, LLint position);
-
-        void RemoveFinish(JobCEC job, LLint position);
-
-        void print();
-    };
-
-    struct JobGroupRange // include the minIndex, but not the maxIndex
-    {
-        JobGroupRange(int mi, int ma) : minIndex(mi), maxIndex(ma) {}
-        int minIndex;
-        int maxIndex;
-    };
-    std::vector<TimeInstance> ExtractSubInstances(const SFOrder &jobOrderCurrForFinish, JobGroupRange &jobGroup);
+  JobGroupRange(int mi, int ma) : minIndex(mi), maxIndex(ma) {}
+  int minIndex;
+  int maxIndex;
+};
+std::vector<TimeInstance> ExtractSubInstances(const SFOrder &jobOrderCurrForFinish, JobGroupRange &jobGroup);
 } // namespace OrderOptDAG_SPACE
---
+++ tests/testFindGlobal.cpp
@@ -0,0 +1,137 @@
+#include "gtsam/base/Testable.h" // assert_equal
+#include "ilcplex/cplex.h"
+#include "ilcplex/ilocplex.h"
+#include "gmock/gmock.h" // Brings in gMock.
+#include <Eigen/Core>
+#include <Eigen/Jacobi>
+#include <Eigen/SparseCore>
+#include <gtest/gtest.h>
+
+#include "sources/Factors/JacobianAnalyze.h"
+#include "sources/Optimization/LinearProgrammingSolver.h"
+#include "sources/Optimization/OrderScheduler.h"
+#include "sources/TaskModel/DAG_Model.h"
+#include "sources/Utils/IncrementQR.h"
+
+using namespace OrderOptDAG_SPACE;
+using namespace OrderOptDAG_SPACE::OptimizeSF;
+using namespace GlobalVariablesDAGOpt;
+using namespace LPOptimizer;
+// TODO: Fix DDL issue, rhs should minus execution time;
+class DAGScheduleOptimizerTest1 : public ::testing::Test {
+protected:
+  void SetUp() override {
+    dagTasks = ReadDAG_Tasks(GlobalVariablesDAGOpt::PROJECT_PATH + "TaskData/test_n3_v30.csv", "orig", 1);
+    tasks = dagTasks.tasks;
+    tasksInfo = TaskSetInfoDerived(tasks);
+    chain1 = {0, 2};
+    dagTasks.chains_[0] = chain1;
+    timeLimits = 1;
+    scheduleOptions.processorNum_ = 2;
+    scheduleOptions.considerSensorFusion_ = 0;
+    scheduleOptions.freshTol_ = 0;
+    scheduleOptions.sensorFusionTolerance_ = 0;
+    scheduleOptions.weightInMpRTDA_ = 0.5;
+    scheduleOptions.weightInMpSf_ = 0.5;
+    scheduleOptions.weightPunish_ = 10;
+
+    initial = GenerateVectorDynamic(3);
+    initial << 0, 15, 12;
+    jobOrder = SFOrder(tasksInfo, initial);
+    jobOrder.print();
+    VectorDynamic initial2 = SFOrderScheduling(dagTasks.tasks, tasksInfo, scheduleOptions.processorNum_,
+                                               jobOrder, processorJobVec);
+  };
+
+  double timeLimits = 1;
+  DAG_Model dagTasks;
+  TaskSet tasks;
+  TaskSetInfoDerived tasksInfo;
+  std::vector<int> chain1;
+  ScheduleOptions scheduleOptions;
+  std::vector<uint> processorJobVec;
+  SFOrder jobOrder;
+  VectorDynamic initial;
+};
+
+class DAGScheduleOptimizerTest2 : public ::testing::Test {
+protected:
+  void SetUp() override {
+    dagTasks = ReadDAG_Tasks(GlobalVariablesDAGOpt::PROJECT_PATH + "TaskData/test_n2_v2.csv", "orig", 1);
+    tasks = dagTasks.tasks;
+    tasksInfo = TaskSetInfoDerived(tasks);
+
+    scheduleOptions.processorNum_ = 2;
+    scheduleOptions.considerSensorFusion_ = 0;
+    scheduleOptions.freshTol_ = 0;
+    scheduleOptions.sensorFusionTolerance_ = 0;
+    scheduleOptions.weightInMpRTDA_ = 0.5;
+    scheduleOptions.weightInMpSf_ = 0.5;
+    scheduleOptions.weightPunish_ = 10;
+  };
+
+  double timeLimits = 1;
+  DAG_Model dagTasks;
+  TaskSet tasks;
+  TaskSetInfoDerived tasksInfo;
+  ScheduleOptions scheduleOptions;
+};
+
+class JobQueueOfATask {
+public:
+  Task task_;
+  size_t jobIndexCurr;
+  size_t maxJobNum;
+  JobQueueOfATask(const Task &task, int maxJobNum) : task_(task), jobIndexCurr(0), maxJobNum(maxJobNum) {}
+
+  bool ReachEnd() const { return jobIndexCurr >= maxJobNum - 1; }
+  bool MoveForward() {
+    if (ReachEnd())
+      return false;
+    jobIndexCurr++;
+    return true;
+  }
+  JobCEC Job() const { return JobCEC(task_.id, jobIndexCurr); }
+};
+
+std::vector<std::vector<TimeInstance>> FindAllJobOrderPermutations(const DAG_Model &dagTasks,
+                                                                   const TaskSetInfoDerived &tasksInfo) {
+  int N = dagTasks.tasks.size();
+  if (N > 3)
+    CoutError("FindAllJobOrderPermutations only works with task sets of 3 DAGs!");
+
+  std::vector<JobQueueOfATask> jobQueueTaskSet;
+  jobQueueTaskSet.reserve(N);
+  for (int i = 0; i < N; i++)
+    jobQueueTaskSet.push_back(
+        JobQueueOfATask(dagTasks.tasks[i], tasksInfo.hyperPeriod / dagTasks.tasks[i].period));
+
+  std::vector<std::vector<TimeInstance>> instOrderAll;
+  instOrderAll.reserve(1000); // should be the max?
+  return instOrderAll;
+}
+
+TEST_F(DAGScheduleOptimizerTest2, FindAllJobOrderPermutations) {
+
+  std::vector<TimeInstance> seq0{
+      {'s', JobCEC(0, 0)}, {'f', JobCEC(0, 0)}, {'s', JobCEC(1, 0)}, {'f', JobCEC(1, 0)}};
+  std::vector<TimeInstance> seq1{
+      {'s', JobCEC(0, 0)}, {'f', JobCEC(0, 0)}, {'s', JobCEC(1, 0)}, {'f', JobCEC(1, 0)}};
+  std::vector<TimeInstance> seq2{
+      {'s', JobCEC(0, 0)}, {'f', JobCEC(0, 0)}, {'s', JobCEC(1, 0)}, {'f', JobCEC(1, 0)}};
+  std::vector<TimeInstance> seq3{
+      {'s', JobCEC(0, 0)}, {'f', JobCEC(0, 0)}, {'s', JobCEC(1, 0)}, {'f', JobCEC(1, 0)}};
+  std::vector<std::vector<TimeInstance>> instSeqAll{seq0, seq1, seq2, seq3};
+
+  std::vector<std::vector<TimeInstance>> instSeqAllActual = FindAllJobOrderPermutations(dagTasks, tasksInfo);
+  EXPECT_EQ(instSeqAll.size(), instSeqAllActual.size());
+  for (uint i = 0; i < instSeqAll.size(); i++) {
+    for (uint j = 0; j < instSeqAll[i].size(); j++)
+      EXPECT_TRUE(instSeqAll[i][j] == instSeqAllActual[i][j]);
+  }
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
