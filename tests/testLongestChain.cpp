86b0f664822719e70ea14e80a95fd462cbc8ffaf
zephyr06 <sen.zephyr.wang@gmail.com>
2023-02-01 20:48:54 -0500

    add testInfluenceJobs.cpp

--- tests/testLongestChain.cpp
+++ tests/testLongestChain.cpp
@@ -164,32 +164,35 @@ TEST_F(RTDATest1, WhetherInfluenceJobSimple) {
   EXPECT_TRUE(WhetherInfluenceJobSimple(JobCEC(1, 0), JobCEC(0, 0), jobGroupMap));
 }
 
-TEST_F(RTDATest1, WhetherInfluenceJobSource) {
-  // arg order: jobCurr, jobChanged
-  EXPECT_FALSE(WhetherInfluenceJobSource(JobCEC(0, 0), JobCEC(0, 1), jobGroupMap, jobOrder, 5, 6, tasksInfo,
-                                         startTimeVector));
-  EXPECT_FALSE(WhetherInfluenceJobSource(JobCEC(0, 0), JobCEC(1, 0), jobGroupMap, jobOrder, 6, 7, tasksInfo,
-                                         startTimeVector));
-  EXPECT_FALSE(WhetherInfluenceJobSource(JobCEC(0, 1), JobCEC(0, 0), jobGroupMap, jobOrder, 2, 3, tasksInfo,
-                                         startTimeVector));
-  EXPECT_TRUE(WhetherInfluenceJobSource(JobCEC(1, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 0, 1, tasksInfo,
-                                        startTimeVector));
-}
-TEST_F(RTDATest1, WhetherInfluenceJobSink) {
-  // arg order: jobCurr, jobChanged
-  EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(0, 0), JobCEC(0, 1), jobGroupMap, jobOrder, 5, 6, tasksInfo,
-                                       startTimeVector));
-  EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(0, 0), JobCEC(1, 0), jobGroupMap, jobOrder, 6, 7, tasksInfo,
-                                       startTimeVector));
-  EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(0, 1), JobCEC(0, 0), jobGroupMap, jobOrder, 2, 3, tasksInfo,
-                                       startTimeVector));
-  EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(1, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 0, 1, tasksInfo,
-                                       startTimeVector));
-  EXPECT_TRUE(WhetherInfluenceJobSink(JobCEC(1, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 6, 7, tasksInfo,
-                                      startTimeVector));
-  EXPECT_TRUE(WhetherInfluenceJobSink(JobCEC(0, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 2, 3, tasksInfo,
-                                      startTimeVector));
-}
+// TEST_F(RTDATest1, WhetherInfluenceJobSource) {
+//   // arg order: jobCurr, jobChanged
+//   EXPECT_FALSE(WhetherInfluenceJobSource(JobCEC(0, 0), JobCEC(0, 1), jobGroupMap, jobOrder, 5, 6,
+//   tasksInfo,
+//                                          startTimeVector));
+//   EXPECT_FALSE(WhetherInfluenceJobSource(JobCEC(0, 0), JobCEC(1, 0), jobGroupMap, jobOrder, 6, 7,
+//   tasksInfo,
+//                                          startTimeVector));
+//   EXPECT_FALSE(WhetherInfluenceJobSource(JobCEC(0, 1), JobCEC(0, 0), jobGroupMap, jobOrder, 2, 3,
+//   tasksInfo,
+//                                          startTimeVector));
+//   EXPECT_TRUE(WhetherInfluenceJobSource(JobCEC(1, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 0, 1, tasksInfo,
+//                                         startTimeVector));
+// }
+// TEST_F(RTDATest1, WhetherInfluenceJobSink) {
+//   // arg order: jobCurr, jobChanged
+//   EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(0, 0), JobCEC(0, 1), jobGroupMap, jobOrder, 5, 6, tasksInfo,
+//                                        startTimeVector));
+//   EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(0, 0), JobCEC(1, 0), jobGroupMap, jobOrder, 6, 7, tasksInfo,
+//                                        startTimeVector));
+//   EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(0, 1), JobCEC(0, 0), jobGroupMap, jobOrder, 2, 3, tasksInfo,
+//                                        startTimeVector));
+//   EXPECT_FALSE(WhetherInfluenceJobSink(JobCEC(1, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 0, 1, tasksInfo,
+//                                        startTimeVector));
+//   EXPECT_TRUE(WhetherInfluenceJobSink(JobCEC(1, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 6, 7, tasksInfo,
+//                                       startTimeVector));
+//   EXPECT_TRUE(WhetherInfluenceJobSink(JobCEC(0, 0), JobCEC(2, 0), jobGroupMap, jobOrder, 2, 3, tasksInfo,
+//                                       startTimeVector));
+// }
 
 TEST_F(RTDATest2, WhetherInfluenceJobSimple) {
   EXPECT_TRUE(WhetherInfluenceJobSimple(JobCEC(0, 0), JobCEC(0, 1), jobGroupMap));
@@ -233,7 +236,6 @@ TEST_F(JobPositionTest, UpdateAfterRemoveInstance) {
   EXPECT_EQ(10, jobPosition.start_);
   EXPECT_EQ(15, jobPosition.finish_);
 }
-
 class RTDATest4 : public RTDATest1 {
   void SetUp() override {
     std::string taskSetName = "test_n3_v40";
@@ -256,6 +258,8 @@ TEST_F(RTDATest4, whether_break_chain) {
   EXPECT_TRUE(WhetherInfluenceJobSink(sinkJob, jobRelocate, jobGroupMap_, jobOrder, startP, finishP,
                                       tasksInfo, startTimeVector));
 }
+
+// *******************************************************
 
 class RTDATest5 : public RTDATest1 {
   void SetUp() override {
@@ -287,95 +291,7 @@ class RTDATest6 : public RTDATest1 {
     jobOrder.print();
   }
 };
-
-class RTDATest7 : public RTDATest1 {
-  void SetUp() override {
-    std::string taskSetName = "test_n3_v43";
-    SetUpTaskSet(taskSetName);
-    startTimeVector = GenerateVectorDynamic(8);
-    startTimeVector << 4592, 0, 2000, 4680, 6000, 8000, 782, 6000;
-    jobOrder = SFOrder(tasksInfo, startTimeVector);
-    jobOrder.print();
-    jobGroupMap = ExtractIndependentJobGroups(jobOrder, tasksInfo);
-  }
-};
-
-// previous adjacent job means the jobs whose finish time equals the start time of jobCurr
-std::vector<JobCEC> FindPrevAdjacentJob(JobCEC job, SFOrder &jobOrder,
-                                        const RegularTaskSystem::TaskSetInfoDerived &tasksInfo,
-                                        const VectorDynamic &startTimeVector) {
-  // LLint jobCurrOldStart = jobOrder.GetJobStartInstancePosition(job);
-  double startTime = GetStartTime(job, startTimeVector, tasksInfo);
-  std::vector<JobCEC> prevAdjacentJobs;
-  prevAdjacentJobs.reserve(4 * 2); // actually, cannot be more than #core*2
-
-  std::unordered_set<JobCEC> record;
-  record.reserve(4 * 2);
-  auto AddAdjacentJob = [&](LLint index) {
-    for (int i = index - 1; i >= 0; i--) {
-      TimeInstance instCurr = jobOrder[i];
-      if (instCurr.type == 'f') {
-        JobCEC jobCurr = instCurr.job;
-        double jobCurrfinishTime = GetFinishTime(jobCurr, startTimeVector, tasksInfo);
-        if (std::abs(jobCurrfinishTime - startTime) < 1e-3) {
-          if (record.find(jobCurr) == record.end()) {
-            prevAdjacentJobs.push_back(jobCurr);
-            record.insert(jobCurr);
-          }
-        } else {
-          break;
-        }
-      }
-    }
-  };
-
-  AddAdjacentJob(jobOrder.GetJobFinishInstancePosition(job));
-  AddAdjacentJob(jobOrder.GetJobStartInstancePosition(job));
-  return prevAdjacentJobs;
-}
-TEST_F(RTDATest7, FindPrevAdjacentJob) {
-  auto prevAdjacentJobs = FindPrevAdjacentJob(JobCEC(0, 0), jobOrder, tasksInfo, startTimeVector);
-  EXPECT_EQ(1, prevAdjacentJobs.size());
-  EXPECT_TRUE(JobCEC(2, 0) == prevAdjacentJobs[0]);
-}
-
-// we need to exam all the jobs that are closely adjacent to jobCurr;
-bool WhetherInfluenceJobAndAfterSink(JobCEC jobCurr, const JobCEC &jobChanged,
-                                     std::unordered_map<JobCEC, int> &jobGroupMap, SFOrder &jobOrder,
-                                     LLint startP, LLint finishP,
-                                     const RegularTaskSystem::TaskSetInfoDerived &tasksInfo,
-                                     const VectorDynamic &startTimeVector) {
-  // Exam whether the jobs that are closely adjacent to jobCurr will be influenced by jobChanged
-  if (WhetherInfluenceJobSource(jobCurr, jobChanged, jobGroupMap, jobOrder, startP, finishP, tasksInfo,
-                                startTimeVector))
-    return true;
-
-  // Termination conditions
-  double jobCurrStartTime = GetStartTime(jobCurr, startTimeVector, tasksInfo);
-  if (jobCurrStartTime == GetActivationTime(jobCurr, tasksInfo))
-    return false;
-
-  std::vector<JobCEC> prevAdjacentJob = FindPrevAdjacentJob(jobCurr, jobOrder, tasksInfo, startTimeVector);
-  for (auto job : prevAdjacentJob) {
-    if (WhetherInfluenceJobAndAfterSink(job, jobChanged, jobGroupMap, jobOrder, startP, finishP, tasksInfo,
-                                        startTimeVector))
-      return true;
-  }
-
-  return false;
-}
-
-TEST_F(RTDATest7, WhetherInfluenceJobAndAfterSink) {
-  LLint startP = 0;
-  LLint finishP = 2;
-  EXPECT_TRUE(WhetherInfluenceJobSink(JobCEC(2, 0), JobCEC(1, 0), jobGroupMap, jobOrder, startP, finishP,
-                                      tasksInfo, startTimeVector));
-
-  EXPECT_TRUE(WhetherInfluenceJobAndAfterSink(JobCEC(2, 0), JobCEC(1, 0), jobGroupMap, jobOrder, startP,
-                                              finishP, tasksInfo, startTimeVector));
-  EXPECT_TRUE(WhetherInfluenceJobAndAfterSink(JobCEC(0, 0), JobCEC(1, 0), jobGroupMap, jobOrder, startP,
-                                              finishP, tasksInfo, startTimeVector));
-}
+// TODO:
 // TEST_F(RTDATest6, FindLongestCAChain) {
 //   LongestCAChain longestChain(dagTasks, tasksInfo, jobOrder, startTimeVector,
 //   scheduleOptions.processorNum_); EXPECT_EQ(2, longestChain.size());
