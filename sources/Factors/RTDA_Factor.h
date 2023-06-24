1332197bb270f086b7667d3e1995659bd8682f92
zephyr06 <sen.zephyr.wang@gmail.com>
2023-06-24 12:18:28 +0800

    add FindFirstReactJob and FindLastReadingJob

--- sources/Factors/RTDA_Factor.h
+++ sources/Factors/RTDA_Factor.h
@@ -5,46 +5,53 @@
 #include "sources/Optimization/SFOrder.h"
 #include "sources/Utils/JobCEC.h"
 
-namespace OrderOptDAG_SPACE
-{
-  struct RTDA
-  {
+namespace OrderOptDAG_SPACE {
+struct RTDA {
     double reactionTime;
     double dataAge;
     RTDA() : reactionTime(-1), dataAge(-1) {}
     RTDA(double r, double d) : reactionTime(r), dataAge(d) {}
-    void print()
-    {
-      std::cout << "Reaction time is " << reactionTime << ", data age is " << dataAge << std::endl;
+    void print() {
+        std::cout << "Reaction time is " << reactionTime << ", data age is "
+                  << dataAge << std::endl;
     }
-  };
+};
 
-  RTDA GetMaxRTDA(const std::vector<RTDA> &resVec);
+RTDA GetMaxRTDA(const std::vector<RTDA> &resVec);
 
-  std::vector<RTDA> GetRTDAFromSingleJob(const TaskSetInfoDerived &tasksInfo,
-                                         const std::vector<int> &causeEffectChain, const VectorDynamic &x,
-                                         double precision = GlobalVariablesDAGOpt::NumericalPrecision);
+std::vector<RTDA> GetRTDAFromSingleJob(
+    const TaskSetInfoDerived &tasksInfo,
+    const std::vector<int> &causeEffectChain, const VectorDynamic &x,
+    double precision = GlobalVariablesDAGOpt::NumericalPrecision);
 
-  std::vector<RTDA> GetRTDAFromSingleJob(const TaskSetInfoDerived &tasksInfo,
-                                         const std::vector<int> &causeEffectChain, const gtsam::Values &x);
+std::vector<RTDA> GetRTDAFromSingleJob(const TaskSetInfoDerived &tasksInfo,
+                                       const std::vector<int> &causeEffectChain,
+                                       const gtsam::Values &x);
 
-  RTDA GetMaxRTDA(const TaskSetInfoDerived &tasksInfo, const std::vector<int> &causeEffectChain,
-                  const VectorDynamic &startTimeVector);
+RTDA GetMaxRTDA(const TaskSetInfoDerived &tasksInfo,
+                const std::vector<int> &causeEffectChain,
+                const VectorDynamic &startTimeVector);
 
-  double ObjRTDA(const RTDA &rtda);
-  double ObjRTDA(const std::vector<RTDA> &rtdaVec);
-  double ObjRT(const std::vector<RTDA> &rtdaVec);
-  double ObjDA(const std::vector<RTDA> &rtdaVec);
+double ObjRTDA(const RTDA &rtda);
+double ObjRTDA(const std::vector<RTDA> &rtdaVec);
+double ObjRT(const std::vector<RTDA> &rtdaVec);
+double ObjDA(const std::vector<RTDA> &rtdaVec);
 
-  std::unordered_map<JobCEC, std::vector<JobCEC>>
-  GetRTDAReactChainsFromSingleJob(const TaskSetInfoDerived &tasksInfo, const std::vector<int> &causeEffectChain,
-                                  const VectorDynamic &x);
+std::unordered_map<JobCEC, std::vector<JobCEC>> GetRTDAReactChainsFromSingleJob(
+    const TaskSetInfoDerived &tasksInfo,
+    const std::vector<int> &causeEffectChain, const VectorDynamic &x);
 
-  std::unordered_map<JobCEC, std::vector<JobCEC>>
-  GetReactionChainMap(const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo, SFOrder &jobOrder,
-                      int processorNum, const std::vector<int> &causeEffectChain, int chainIndex);
-  std::unordered_map<JobCEC, std::vector<JobCEC>>
-  GetDataAgeChainMap(const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo, SFOrder &jobOrder,
-                     int processorNum, const std::vector<int> &causeEffectChain, int chainIndex);
+JobCEC FindFirstReactJob(JobCEC source, int next_task_id, SFOrder &jobOrder) ;
+JobCEC FindLastReadingJob(JobCEC sink_job, int source_task_id,
+                          SFOrder &jobOrder,
+                          const TaskSetInfoDerived &tasksInfo);
+std::unordered_map<JobCEC, std::vector<JobCEC>> GetReactionChainMap(
+    const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo,
+    SFOrder &jobOrder, int processorNum,
+    const std::vector<int> &causeEffectChain, int chainIndex);
+std::unordered_map<JobCEC, std::vector<JobCEC>> GetDataAgeChainMap(
+    const DAG_Model &dagTasks, const TaskSetInfoDerived &tasksInfo,
+    SFOrder &jobOrder, int processorNum,
+    const std::vector<int> &causeEffectChain, int chainIndex);
 
-} // namespace OrderOptDAG_SPACE
+}  // namespace OrderOptDAG_SPACE
