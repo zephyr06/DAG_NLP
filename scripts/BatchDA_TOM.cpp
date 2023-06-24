19cd8a9273f12759a258db3f2f6b25747b4f0e16
zephyr06 <sen.zephyr.wang@gmail.com>
2023-06-24 14:22:44 +0800

    update Batch scripts

---
+++ scripts/BatchDA_TOM.cpp
@@ -0,0 +1,8 @@
+#include "sources/batchOptimizeSFOrder.h"
+
+int main() {
+    std::vector<OrderOptDAG_SPACE::BaselineMethods> baselineMethods = {
+        OrderOptDAG_SPACE::InitialMethod, OrderOptDAG_SPACE::TOM};
+    OrderOptDAG_SPACE::BatchOptimizeOrder<
+        OrderOptDAG_SPACE::OptimizeSF::DataAgeObj>(baselineMethods);
+}
