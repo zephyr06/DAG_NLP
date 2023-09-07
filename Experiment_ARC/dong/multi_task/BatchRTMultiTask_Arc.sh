ea24d62cb49a7aefe9ab9efa39df082dd8455139
Dong Li <dongli@vt.edu>
2023-09-07 16:39:49 -0400

    update ARC scripts

--- Experiment_ARC/dong/multi_task/BatchRTMultiTask_Arc.sh
+++ Experiment_ARC/dong/multi_task/BatchRTMultiTask_Arc.sh
@@ -18,7 +18,7 @@ perform_optimization() {
 	## test in local
 	# bash SBatchRTMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
 	## launc on ARC
-	# sbatch -J BatchRT_${task_number} --nodes=1 --ntasks-per-node=$(((MaxFileIndex+files_per_task)/files_per_task)) --cpus-per-task=1 --time ${time_per_node}:0:0 --output=${output_file_name} SBatchRTMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
+	sbatch -J BatchRT_${task_number} --nodes=1 --ntasks-per-node=$(((MaxFileIndex+files_per_task)/files_per_task)) --cpus-per-task=1 --time ${time_per_node}:0:0 --output=${output_file_name} SBatchRTMultiTask.sh $1 $MinFileIndex $files_per_task $MaxFileIndex
 }
 
 
