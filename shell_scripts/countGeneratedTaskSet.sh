323ffb1e5e1417a46b611012a842cbcebd5bf8b9
Dong Li <dongli@vt.edu>
2023-09-07 16:47:02 -0400

    update scripts

--- shell_scripts/countGeneratedTaskSet.sh
+++ shell_scripts/countGeneratedTaskSet.sh
@@ -2,6 +2,7 @@
 COUNT_PATH="/home/dong/workspace/DAG_NLP/TaskData"
 # COUNT_PATH=/home/zephyr/Programming/DAG_NLP/TaskData
 # COUNT_PATH="/home/dong/workspace/DAG_NLP/release/generatedNewTaskset"
+COUNT_PATH="/projects/rtss_let/dong/DAG_NLP/TaskData"
 
 echo current results count in dir: $COUNT_PATH
 
