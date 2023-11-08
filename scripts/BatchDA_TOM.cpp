705498c3caa2b6c3694d88794f16b87740706c5b
Dong Li <dongli@vt.edu>
2023-11-08 00:59:06 -0500

    update batch scripts

--- scripts/BatchDA_TOM.cpp
+++ scripts/BatchDA_TOM.cpp
@@ -40,7 +40,8 @@ int main(int argc, char *argv[]) {
         OrderOptDAG_SPACE::TOM_Fast,
         OrderOptDAG_SPACE::TOM, 
         // OrderOptDAG_SPACE::TOM_IA,
-        OrderOptDAG_SPACE::TOM_Threshold};
+        OrderOptDAG_SPACE::TOM_Threshold,
+        OrderOptDAG_SPACE::TOM_RandomSearch};
         // OrderOptDAG_SPACE::Verucchi20};
     OrderOptDAG_SPACE::BatchOptimizeOrder<
         OrderOptDAG_SPACE::OptimizeSF::DataAgeObj>(baselineMethods, batch_test_settings);
